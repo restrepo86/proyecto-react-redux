6c630e105ed069d9d439d5dbfcb7f778a8dfb2bd
Juan Fernando Restrepo <JuanRestrepo>
2020-05-15 11:12:17 -0500

    Se agregan estilos al formulario

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,61 @@
-import React from 'react';
-import { Container, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Container, Typography, Card, Grid, TextField, Button } from '@material-ui/core';
 
+import styles from './style';
 
-export default () => (<Container><Typography variant='h4'>Home</Typography></Container>)
+export default () => {
+    
+    const [searchText, setSearchText] = useState('');
+
+    const classes = styles();
+
+    const handleSearchTextChange = event => {
+        setSearchText(event.target.value);
+    };
+
+    const handleCleanTextClick = event => {
+
+    };
+
+    const handleSearchTextClick = event => {
+
+    };    
+
+    return(
+        <Container className={classes.container}>
+            <Card className={classes.cardContainer}>
+                <Grid container className={classes.titleGridContainer}>
+                    <Grid>
+                        <Typography className={classes.title}>Bienvenido!</Typography>
+                    </Grid>
+                    <Grid>
+                        <label>Icono</label>
+                    </Grid>
+                </Grid>
+                <TextField
+                    value = { searchText }
+                    placeholder = 'Buscar...'
+                    className = { classes.textFieldSearch}
+                    onChange = { handleSearchTextChange }
+                />
+                <Grid className = { classes.buttonsContainer }>
+                    <Button 
+                        variant='contained' 
+                        onClick={ handleCleanTextClick }
+                    >
+                        Limpiar
+                    </Button>
+                    <Button 
+                        variant='contained' 
+                        color='primary' 
+                        size='large' 
+                        className = { classes.searchButton }
+                        onClick={ handleSearchTextClick }
+                    >
+                        Buscar
+                    </Button>
+                </Grid>
+            </Card>
+        </Container>
+    );
+};
