51b3d7cd4fd6ef91cb7119a9f6c9a938cf88ff15
Juan Fernando Restrepo <JuanRestrepo>
2020-05-15 10:37:27 -0500

    Se agrega MaterialUI al Proyecto

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,5 @@
 import React from 'react';
+import { Container, Typography } from '@material-ui/core';
 
-export default () => (<div>Home</div>)
+
+export default () => (<Container><Typography variant='h4'>Home</Typography></Container>)
